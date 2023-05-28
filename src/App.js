2f425eda94326a9e14723ebdcf3b5f20023ed225
MerkhatM <merhatmanlaev@gmail.com>
2023-05-28 19:51:02 +0600

    demo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import { Box } from '@mui/material';
+import {Route, Routes } from 'react-router-dom';
 
-function App() {
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ExcerciseDetail from './pages/ExcersiceDetail';
+import BmiCalculator from './pages/BmiCalculator';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Box width="400px" sx={ {width: { xl: '1488px'} } } m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExcerciseDetail />} />
+          <Route path="/BmiCalculator" element={<BmiCalculator />} />
+        </Routes>
+        <Footer />
+      </Box>
+  )
 }
 
 export default App;
+//
+//
+// export const exerciseOptions =  {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+//     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+//   }
+// };
+//
+// export const youtubeOptions =  {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+//     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+//   }
+// };
+//
+// export const BMIOptions = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+//     'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+//   }
+// };
+//
+//
+//
+// export const fetchData = async (url, options) => {
+//   const response  = await fetch(url, options);
+//   const data = await response.json();
+//   return data;
+// };
---
+++ src/utils/fetchData.js
@@ -0,0 +1,32 @@
+
+export const exerciseOptions =  {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+
+export const youtubeOptions =  {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+        'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+};
+
+export const BMIOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '1144e44640msh866aeea4e26f4fbp129e3cjsnabc82c7aa74f',
+        'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
+    }
+};
+
+
+
+export const fetchData = async (url, options) => {
+    const response  = await fetch(url, options);
+    const data = await response.json();
+    return data;
+};
